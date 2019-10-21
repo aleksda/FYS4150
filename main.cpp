b7487652087f9f346aa18beb4f4842d877f62cca
samuelbrandt <49906218+samuelbrandt@users.noreply.github.com>
2019-10-21 12:48:00 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <g_legendre.h>
+#include <g_laguerre.h>
+
+using namespace std;
+
+int main()
+{
+    double pi = 3.1415;
+    gauss_laguerre(0,2,0,pi,0,2*pi,20);
+
+    return 0;
+}
